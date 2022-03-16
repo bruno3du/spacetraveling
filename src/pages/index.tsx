0839f3edb27d8dead3a3230960ed058e59f9e445
Bruno@Eduardo <bruno.3duardo@hotmail.com>
2022-03-16 14:01:21 -0300

    blog working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
-
+import Head from 'next/head';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { predicate } from '@prismicio/client';
 import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
@@ -34,20 +35,18 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
-  const [posts, setPosts] = useState([] as Post[]);
-  const [nextPage, setNextPage] = useState<string>(null);
+  const [posts, setPosts] = useState(postsPagination.posts as Post[]);
+  const [nextPage, setNextPage] = useState<string>(
+    postsPagination.next_page || null
+  );
 
-  useEffect(() => {
-    if (postsPagination?.posts) setPosts(postsPagination.posts);
-    if (postsPagination?.next_page) setNextPage(postsPagination.next_page);
-  }, []);
-
-  function handleDate(date): string {
-    const newDate = format(date, 'PP', {
+  function handleDate(date: string): string {
+    const newDate = new Date(date);
+    const formatedDate = format(newDate, 'PP', {
       locale: ptBR,
     });
 
-    return newDate;
+    return formatedDate;
   }
 
   async function handleMoreArticles(api: string): Promise<void> {
@@ -56,8 +55,6 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
     const { results, ...rest } = data;
     const { next_page } = rest;
-
-    console.log(results);
 
     await results.forEach((ele: Post) => {
       const newPost = {
@@ -75,55 +72,61 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
     setNextPage(next_page);
   }
-  console.log(posts);
+
   return (
-    <div className={commonStyles.container}>
-      <Header />
-      <main className={styles.container}>
-        <section>
-          {posts.map((post, index) => (
-            <div key={`${index + 2} postX`} className={styles.posts}>
-              <Link
-                href={{
-                  pathname: '/post/[slug]',
-                  query: { slug: post.uid },
-                }}
-                passHref
-              >
-                <a>
-                  <h2>{post?.data?.title}</h2>
-                </a>
-              </Link>
-              <p>{post?.data?.subtitle}</p>
-              <div className={styles.posts_footer}>
-                <div className={styles.posts_footer__time}>
-                  <FiCalendar />
-                  <time>{handleDate(new Date())}</time>
-                </div>
-                <div className={styles.posts_footer__author}>
-                  <FiUser />
-                  <p>{post?.data?.author}</p>
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+      <div className={commonStyles.container}>
+        <Header />
+        <main className={styles.container}>
+          <section>
+            {posts.map((post, index) => (
+              <div key={`${index + 2} postX`} className={styles.posts}>
+                <Link
+                  href={{
+                    pathname: '/post/[slug]',
+                    query: { slug: post.uid },
+                  }}
+                  passHref
+                >
+                  <a>
+                    <h2>{post?.data?.title}</h2>
+                  </a>
+                </Link>
+                <p>{post?.data?.subtitle}</p>
+                <div className={styles.posts_footer}>
+                  <div className={styles.posts_footer__time}>
+                    <FiCalendar />
+                    <time>{handleDate(post.first_publication_date)}</time>
+                  </div>
+                  <div className={styles.posts_footer__author}>
+                    <FiUser />
+                    <p>{post?.data?.author}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </section>
-        {nextPage && (
-          <button type="button" onClick={() => handleMoreArticles(nextPage)}>
-            Carregar mais posts
-          </button>
-        )}
-      </main>
-    </div>
+            ))}
+          </section>
+          {nextPage && (
+            <button type="button" onClick={() => handleMoreArticles(nextPage)}>
+              Carregar mais posts
+            </button>
+          )}
+        </main>
+      </div>
+    </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const postsResponse = await prismic.getByType('post', {
-    pageSize: 1,
-  });
+  const postsResponse = await prismic.query(
+    [predicate.at('document.type', 'post')],
+    { pageSize: 1 }
+  );
 
   const { results, ...postRest } = postsResponse;
 
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,6 +3,9 @@ import { format } from 'date-fns';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 import Image from 'next/image';
 import ptBR from 'date-fns/locale/pt-BR';
+import { predicate } from '@prismicio/client';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -20,9 +23,7 @@ interface Post {
     author: string;
     content: {
       heading: string;
-      body: {
-        text: string;
-      }[];
+      body: RichTextBlock[];
     }[];
   };
 }
@@ -31,19 +32,38 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
-  function handleDate(date): string {
-    const newDate = format(date, 'PP', {
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  function handleDate(date: string): string {
+    const newDate = new Date(date);
+    const formatedDate = format(newDate, 'PP', {
       locale: ptBR,
     });
 
-    return newDate;
+    return formatedDate;
   }
 
   function handleReadingTime(words): number {
+    let amountWords = 0;
+    words.forEach(word => {
+      const newWord = RichText.asText(word.body);
+      const numberWords = newWord.split(' ').length;
+      amountWords += numberWords;
+    });
+
     const wordPerMinuts = 200;
 
-    return Math.ceil(words / wordPerMinuts);
+    return Math.ceil(amountWords / wordPerMinuts);
+  }
+
+  if (router.isFallback) {
+    return (
+      <>
+        <Header />
+        <main className={styles.container}>Carregando...</main>
+      </>
+    );
   }
 
   return (
@@ -54,135 +74,27 @@ export default function Post(): JSX.Element {
           <Image src="/post.png" alt="post" layout="fill" objectFit="cover" />
         </figure>
         <article className={`${styles.post} ${commonStyles.container}`}>
-          <h1>Criando um app CRA do zero</h1>
+          <h1>{post.data.title}</h1>
           <div className={styles.post_info}>
             <div>
               <FiCalendar />
-              {handleDate(new Date())}
+              <time>{handleDate(post.first_publication_date)}</time>
             </div>
             <div>
               <FiUser />
-              Joseph Oliveira
+              {post.data.author}
             </div>
             <div>
-              <FiClock /> {handleReadingTime(950)} min
+              <FiClock /> {handleReadingTime(post.data.content)} min
             </div>
           </div>
           <div className={styles.post_content}>
-            <h2>Proin et varius</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              dolor sapien, vulputate eu diam at, condimentum hendrerit tellus.
-            </p>
-            <p>
-              Nam facilisis sodales felis, pharetra pharetra lectus auctor sed.
-              Ut venenatis mauris vel libero pretium, et pretium ligula
-              faucibus.
-            </p>
-            <p>
-              Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam
-              venenatis.
-            </p>
-            <h2>Cras laoreet mi</h2>
-
-            <p>
-              Nulla auctor sit amet quam vitae commodo. Sed risus justo,
-              vulputate quis neque eget, dictum sodales sem. In eget felis
-              finibus, mattis magna a, efficitur ex. Curabitur vitae justo
-              consequat sapien gravida auctor a non risus. Sed malesuada mauris
-              nec orci congue, interdum efficitur urna dignissim. Vivamus cursus
-              elit sem, vel facilisis nulla pretium consectetur. Nunc congue.
-            </p>
-
-            <p>
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos. Aliquam consectetur massa nec
-              metus condimentum, sed tincidunt enim tincidunt. Vestibulum
-              fringilla risus sit amet massa suscipit eleifend. Duis eget metus
-              cursus, suscipit ante ac, iaculis est. Donec accumsan enim sit
-              amet lorem placerat, eu dapibus ex porta. Etiam a est in leo
-              pulvinar auctor. Praesent sed vestibulum elit, consectetur egestas
-              libero.
-            </p>
-
-            <p>
-              Ut varius quis velit sed cursus. Nunc libero ante, hendrerit eget
-              consectetur vel, viverra quis lectus. Sed vulputate id quam nec
-              tristique. Etiam lorem purus, imperdiet et porta in, placerat non
-              turpis. Cras pharetra nibh eu libero ullamcorper, at convallis
-              orci egestas. Fusce ut est tellus. Donec ac consectetur magna, nec
-              facilisis enim. Sed vel tortor consectetur, facilisis felis non,
-              accumsan risus. Integer vel nibh et turpis.
-            </p>
-
-            <p>
-              Nam eu sollicitudin neque, vel blandit dui. Aliquam luctus aliquet
-              ligula, sed:
-            </p>
-
-            <ul>
-              <li>
-                Suspendisse ac facilisis leo. Sed nulla odio, aliquam ut
-                lobortis vitae, viverra quis risus. Vivamus pulvinar enim sit
-                amet elit porttitor bibendum. Nulla facilisi. Aliquam libero
-                libero, porta ac justo vitae, dapibus convallis sapien. Praesent
-                a nibh pretium, ultrices urna eget, vulputate felis. Phasellus
-                ac sagittis ipsum, a congue lectus. Integer interdum ut velit
-                vehicula volutpat. Nulla facilisi. Nulla rhoncus metus lorem,
-                sit amet facilisis ipsum faucibus et. Lorem ipsum.
-              </li>
-              <li>
-                Curabitur a rutrum ante. Praesent in justo sagittis, dignissim
-                quam facilisis, faucibus dolor. Vivamus sapien diam, faucibus
-                sed sodales sed, tincidunt quis sem. Donec tempus ipsum massa,
-                ut fermentum ante molestie consectetur. In hac habitasse platea
-                dictumst. Sed non finibus nibh, vitae dapibus arcu. Sed lorem
-                magna, imperdiet non pellentesque et, rhoncus ac enim. Class
-                aptent taciti sociosqu ad litora torquent per conubia.
-              </li>
-            </ul>
-
-            <p>
-              Praesent ac sapien eros. Suspendisse potenti. Morbi eu ante nibh.
-              Proin dictum, tellus ut molestie tincidunt, urna tortor sodales
-              velit, ut tempor lectus ipsum nec sapien. Nulla nec purus vitae
-              libero aliquet posuere non et sapien. Cras in erat rhoncus,
-              dignissim ligula iaculis, faucibus orci. Donec ligula neque,
-              imperdiet vitae mauris eget, egestas varius massa. Praesent ornare
-              nisi at dui dapibus, ac tristique felis.
-            </p>
-
-            <p>
-              Phasellus maximus urna lacus, non imperdiet ex blandit sit amet.
-              Vivamus et tellus est. Mauris ligula elit, placerat non tellus a,
-              dictum porttitor urna. Phasellus mollis turpis id suscipit
-              dapibus. In dolor.
-            </p>
-
-            <p>
-              Sed sit amet euismod sapien, non eleifend erat. Vivamus et quam
-              odio. Integer nisi lacus, maximus sit amet turpis in, luctus
-              molestie sem. Duis sit amet euismod erat. Fusce pulvinar ex neque,
-              egestas cursus nulla ullamcorper vel. Pellentesque mollis erat
-              egestas est rhoncus, sit amet sodales massa ullamcorper. Etiam
-              auctor ante a neque facilisis tristique. Proin ultricies fringilla
-              turpis, eget tempus elit imperdiet non. Quisque.
-            </p>
-
-            <p>
-              Etiam eu tortor placerat, varius orci non, ornare nunc. Cras
-              suscipit in ligula ultricies lacinia. Pellentesque at tristique
-              sapien, et scelerisque leo. Donec eu nisi at magna tristique
-              luctus vel at turpis. Nam vestibulum ornare ex cursus vulputate.
-              In elementum tellus at sapien bibendum, id maximus mauris
-              convallis. Donec facilisis porta lobortis. Vivamus mauris diam,
-              pretium ac dolor.
-            </p>
-
-            <p>
-              Pellentesque et consequat arcu, ac laoreet ante.
-              <strong> Nam non</strong>.
-            </p>
+            {post.data.content.map(content => (
+              <div key={content.heading}>
+                <h2>{content.heading}</h2>
+                <RichText render={content.body} />
+              </div>
+            ))}
           </div>
         </article>
       </main>
@@ -190,16 +102,32 @@ export default function Post(): JSX.Element {
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const postsFetch = await prismic.query([
+    predicate.at('document.type', 'post'),
+  ]);
 
-//   // TODO
-// };
+  const post = postsFetch.results.map(ele => {
+    return {
+      params: { slug: ele.uid },
+    };
+  });
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: post,
+    fallback: true,
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const post = await prismic.getByUID('post', slug as string);
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
